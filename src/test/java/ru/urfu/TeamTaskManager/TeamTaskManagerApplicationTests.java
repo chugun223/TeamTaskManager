63799b081b223b50d0141ea3963cb473c8c81c64
Тимофей Князев <timofeyknyazev@mail.ru>
2026-03-01 16:27:29 +0500

    First commit

---
+++ src/test/java/ru/urfu/TeamTaskManager/TeamTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.TeamTaskManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
