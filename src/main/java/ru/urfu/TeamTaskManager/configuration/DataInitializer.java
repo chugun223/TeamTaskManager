3939902b6973a974b50a8fe279aea17e45351965
chugunenok223 <timofeyknyazev@mail.ru>
2026-03-30 21:59:39 +0500

    Найстройка бд

---
+++ src/main/java/ru/urfu/TeamTaskManager/configuration/DataInitializer.java
@@ -0,0 +1,62 @@
+package ru.urfu.TeamTaskManager.configuration;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.urfu.TeamTaskManager.domain.Task;
+import ru.urfu.TeamTaskManager.domain.Team;
+import ru.urfu.TeamTaskManager.domain.User;
+import ru.urfu.TeamTaskManager.dto.request.CreateTeamRequest;
+import ru.urfu.TeamTaskManager.dto.request.CreateUserRequest;
+import ru.urfu.TeamTaskManager.enums.TaskStatus;
+import ru.urfu.TeamTaskManager.repository.*;
+import ru.urfu.TeamTaskManager.service.*;
+
+import java.time.LocalDateTime;
+
+@Configuration
+public class DataInitializer {
+
+    @Bean
+    CommandLineRunner init(UserRepository userRepository,
+                           TeamRepository teamRepository,
+                           TaskRepository taskRepository,
+                           UserService userService,
+                           TeamService teamService) {
+        return args -> {
+            if (userRepository.count() > 0) {
+                return;
+            }
+
+            User leaderUser = userService.createUser(CreateUserRequest.builder()
+                    .username("leader")
+                    .email("leader@mail.com")
+                    .password("123")
+                    .build());
+
+            User memberUser = userService.createUser(CreateUserRequest.builder()
+                    .username("member")
+                    .email("member@mail.com")
+                    .password("123")
+                    .build());
+
+            Team team = teamService.createTeam(leaderUser.getId(), CreateTeamRequest.builder()
+                    .name("Backend Team")
+                    .description("Java team")
+                    .build());
+
+            userService.assignUserToTeam(memberUser.getId(), team.getId());
+
+            Task task = new Task();
+            task.setTitle("Сделать API");
+            task.setDescription("Написать контроллеры");
+            task.setTaskStatus(TaskStatus.NEW);
+            task.setDeadline(LocalDateTime.now().plusDays(3));
+            task.setAssignedUser(memberUser);
+
+            taskRepository.save(task);
+
+        };
+    }
+}
---
+++ src/main/java/ru/urfu/TeamTaskManager/configuration/EnvConfig.java
@@ -0,0 +1,12 @@
+package ru.urfu.TeamTaskManager.configuration;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EnvConfig {
+    static {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
+}
